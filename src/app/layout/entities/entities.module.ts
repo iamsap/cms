043766ce281637b0a280b65fdf79ea07ae242893
Robby Millsap <robbymillsap@gmail.com>
2019-04-03 16:14:40 -0700

    Initial checkin

---
+++ src/app/layout/entities/entities.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { EntitiesRoutingModule } from './entities-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    EntitiesRoutingModule
+  ]
+})
+export class EntitiesModule { }
